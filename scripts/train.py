003ca7555cd134306f8ca97617ad3512d030ddaa
Joachim Wagner <jowagner@users.noreply.github.com>
2023-01-11 11:46:40 +0000

    draft code for training the model

--- scripts/train.py
+++ scripts/train.py
@@ -8,6 +8,7 @@
 
 # Author: Joachim Wagner
 
+from collections import defaultdict
 from sklearn.naive_bayes import MultinomialNB
 import sys
 
@@ -28,12 +29,12 @@ def main():
             help='Where to find the EDOS data (default: data)',
             )
     parser.add_argument(
-            '--data_augmentation', type=str, default='none',
+            '--augmentation', type=str, default='none',
             help='How to augment the training data;'
                  ' space-separated list of methods;'
                  ' include "exclude_basic" to not include'
                  ' a copy of the basic dataset;'
-                 ' (default: none = no augmentation)',
+                 ' (default: none = no data augmentation)',
             )
     parser.add_argument(
             '--task', type=str, default='a',
@@ -57,6 +58,11 @@ def main():
                  ' and the run (see --run) specifies which part'
                  ' (default: internal)',
             )
+    parser.add_argument(
+            '--min_freq', type=int, default=5,
+            help='Events must have at least this frequency to be included'
+                 ' in the vocabulary (default: 5)',
+            )
     print('Parsing arguments...')
     args = parser.parse_args()
     print('Seeding PRNGs...')
@@ -71,10 +77,12 @@ def main():
         import numpy
         seed = base64.b64encode(numpy.random.bytes(32)).decode('utf-8')
     print('Dateset seed:', seed)
+    #training_data = get_training_data(args)
+    #def get_training_data(args):
     training_data = []
     # allow both underscore and minus in --data-augmentation
-    args.data_augmentation = args.data_augmentation.replace('_', '-')
-    if 'exclude-basic' not in args.data_augmentation:
+    args.augmentation = args.augmentation.replace('_', '-')
+    if 'exclude-basic' not in args.augmentation:
         # usually, we include a copy of the training data as is, i.e.
         # 1 copy of each document with no sampling of sub-units
         training_data.append(EDOSDataset(
@@ -87,10 +95,16 @@ def main():
         ))
     # support alternative delimiters in --data-augmentation
     for delimiter in ',| ':
-        args.data_augmentation = args.data_augmentation.replace(delimiter, '+')
+        args.augmentation = args.augmentation.replace(delimiter, '+')
+    aug2count = defaultdict(lambda: 0)
     for augmentation in args.data_augmentation.split('+'):
         if augmentation in ('none', 'exclude-basic'):
+            # no data to add
             continue
+        # in case the same augmentation is used multiple times, we
+        # need to know this to use a different random seed below
+        aug2count[augmentation] += 1
+        a_seq = aug2count[augmentation]
         if 'per-class-with' in augmentation:  # e.g. 100-per-class-with-3-documents
             fields = augmentation.split('-')
             # [0] [1] [2]   [3] [4] [5]
@@ -98,21 +112,52 @@ def main():
             assert len(fields) == 6
             assert fields[5] == 'documents'
             training_data.append(EDOSDataset(
-                augmentation + ':' + seed,
+                '%s:%d:%s' %(augmentation, a_seq, seed),
                 args.dataset_path, args.run, args.settype + '-training',
                 unit = 'class', items_per_unit = int(fields[0]),
                 fraction_of_subunits = None,
                 number_of_subunits = int(fields[4]),
                 deduplicate = True,
             ))
+        elif augmentation.endswith('-sample-with-replacement'):
+            fields = augmentation.split('-')
+            assert len(fields) == 4
+            target_size = float(fields[0])
+            # TODO: sample with replacement
+            raise NotImplementedError
+        elif augmentation.endswith('-sample-without-replacement'):
+            fields = augmentation.split('-')
+            assert len(fields) == 4
+            target_size = float(fields[0])
+            if target_size >= 1.0:
+                copies = int(target_size)  # rounding down
+                # TODO: append full copies of data
+                target_size -= copies
+            if target_size > 0.0:
+                # TODO: create sample and append to data
+            raise NotImplementedError
         else:
-            raise ValueError('unknown augmentation profile %s' %augmentation)
+            raise ValueError('unknown augmentation %s' %augmentation)
     if len(training_data) == 1:
         training_data = training_data[0]
+    elif len(training_data) == 0:
+        raise ValueError('When using exclude-basic, you must add at least one augmentation method')
     else:
         training_data = Concat(training_data)
 
     print('Number of training items:', len(training_data))
+    internal_model = MultinomialNB()
+    detector = SexismDetectorWithNgrams(
+            task  = args.task,
+            model = internal_model,
+            tokeniser = tokeniser,      # TODO: provide tokeniser
+            min_freq  = args.min_freq,
+            ngram_range = ngram_range,
+            padding     = padding,
+    )
+    detector.train(training_data)
+    # TODO: use pickle module to save detector (not the model as it doesn't know how
+    # to map text to features and label indices to labels)
 
 if __name__ == '__main__':
     main()
