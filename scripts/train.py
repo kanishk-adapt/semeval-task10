kanishk-adapt/semeval-task10

commit c9212a48fd10de263baca9a7d185365c10617132

support feature extraction for dev and test sets

scripts/train.py
@@ -40,6 +40,8 @@ def get_seed(args):
     return seed
 
 def get_training_data(args, seed):
+    if args.settype in ('internal', 'official'):
+        args.settype = args.settype + '-training'
     training_data = []
     # allow both underscore and minus in --augmentation
     args.augmentation = args.augmentation.replace('_', '-')
@@ -49,7 +51,7 @@ def get_training_data(args, seed):
         # unless the user specifies "exclude-basic"
         training_data.append(EDOSDataset(
             'basic:' + seed,
-            args.dataset_path, args.run, args.settype + '-training',
+            args.dataset_path, args.run, args.settype,
             unit = 'document', items_per_unit = 1,
             fraction_of_subunits = None,
             number_of_subunits = None,
@@ -201,7 +203,9 @@ def main():
             '--settype', type=str, default='internal',
             help='Which training set to use: internal or official;'
                  ' "internal" uses 80%% of the official training data'
-                 ' and the run (see --run) specifies which part'
+                 ' and the run (see --run) specifies which part;'
+                 ' can also use dev and test sets,'
+                 ' e.g. for feature extraction'
                  ' (default: internal)',
             )
     parser.add_argument(
@@ -291,6 +295,8 @@ def main():
     # We save the detector as the model doesn't know how
     # to map text to features and label indices to labels
     joblib.dump(detector, path)
+    if '-dev' in args.settype or '-test' in args.settype:
+        print('Warning: model has been trained on dev or test data')
 
 if __name__ == '__main__':
     main()

commit 5e2a3dea66d2913588b0a0b5714d283ead2ce72e

fix regression introduced with commit c9212a48fd10de263baca9a7d185365c10617132

scripts/train.py
@@ -78,7 +78,7 @@ def get_training_data(args, seed):
             assert fields[5] == 'documents'
             training_data.append(EDOSDataset(
                 '%s:%d:%s' %(augmentation, a_seq, seed),
-                args.dataset_path, args.run, args.settype + '-training',
+                args.dataset_path, args.run, args.settype,
                 unit = 'class', items_per_unit = int(fields[0]),
                 fraction_of_subunits = None,
                 number_of_subunits = int(fields[4]),