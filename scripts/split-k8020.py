c843f9536e03aee7da3f656afaf3f2b39f5499f9
Joachim Wagner <jowagner@users.noreply.github.com>
2023-01-19 15:06:51 +0000

    create data splits for cross-validation runs 2 to 5

--- scripts/split-k8020.py
+++ scripts/split-k8020.py
@@ -20,14 +20,67 @@ df = pandas.read_csv(sys.argv[1])
 
 assert len(df) in (14000, 5)
 
+k20len = int(round(len(df) * 0.2))
+k40len = int(round(len(df) * 0.4))
+k60len = int(round(len(df) * 0.6))
 k80len = int(round(len(df) * 0.8))
 
 k8020_tr = df.loc[:k80len-1]   # https://pandas.pydata.org/docs/user_guide/advanced.html#advanced-endpoints-are-inclusive
 k8020_dev = df.loc[k80len:]
 
+k8020_dev_run_1 = k8020_dev
+k8020_dev_run_2 = df.loc[k60len:k80len-1]
+k8020_dev_run_3 = df.loc[k40len:k60len-1]
+k8020_dev_run_4 = df.loc[k20len:k40len-1]
+k8020_dev_run_5 = df.loc[:k20len-1]
+
+
+k8020_tr_run_2 = pandas.concat([
+    k8020_dev_run_1,
+    k8020_dev_run_3,
+    k8020_dev_run_4,
+    k8020_dev_run_5,
+])
+
+k8020_tr_run_3 = pandas.concat([
+    k8020_dev_run_1,
+    k8020_dev_run_2,
+    k8020_dev_run_4,
+    k8020_dev_run_5,
+])
+
+k8020_tr_run_4 = pandas.concat([
+    k8020_dev_run_1,
+    k8020_dev_run_2,
+    k8020_dev_run_3,
+    k8020_dev_run_5,
+])
+
+k8020_tr_run_5 = pandas.concat([
+    k8020_dev_run_1,
+    k8020_dev_run_2,
+    k8020_dev_run_3,
+    k8020_dev_run_4,
+])
+
 print('tr size:', len(k8020_tr), 'expected:', k80len)
+print('  run 2:', len(k8020_tr_run_2))
+print('  run 3:', len(k8020_tr_run_3))
+print('  run 4:', len(k8020_tr_run_4))
+print('  run 5:', len(k8020_tr_run_5))
 
 # https://stackoverflow.com/questions/16923281/writing-a-pandas-dataframe-to-csv-file
-k8020_tr.to_csv( 'k8020-tr.csv',  encoding='utf-8', index=False)
-k8020_dev.to_csv('k8020-dev.csv', encoding='utf-8', index=False)
+k8020_tr.to_csv( 'k8020-tr-run-1.csv',  encoding='utf-8', index=False)
+k8020_dev.to_csv('k8020-dev-run-1.csv', encoding='utf-8', index=False)
 
+k8020_tr_run_2.to_csv( 'k8020-tr-run-2.csv',  encoding='utf-8', index=False)
+k8020_dev_run_2.to_csv('k8020-dev-run-2.csv', encoding='utf-8', index=False)
+
+k8020_tr_run_3.to_csv( 'k8020-tr-run-3.csv',  encoding='utf-8', index=False)
+k8020_dev_run_3.to_csv('k8020-dev-run-3.csv', encoding='utf-8', index=False)
+
+k8020_tr_run_4.to_csv( 'k8020-tr-run-4.csv',  encoding='utf-8', index=False)
+k8020_dev_run_4.to_csv('k8020-dev-run-4.csv', encoding='utf-8', index=False)
+
+k8020_tr_run_5.to_csv( 'k8020-tr-run-5.csv',  encoding='utf-8', index=False)
+k8020_dev_run_5.to_csv('k8020-dev-run-5.csv', encoding='utf-8', index=False)
