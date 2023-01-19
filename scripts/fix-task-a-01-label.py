d7f4b4e836fe90c3bd62b8a48db6df40f20e85a2
Joachim Wagner <jowagner@users.noreply.github.com>
2023-01-19 17:16:53 +0000

    also remove unexpected columns when fixing EDOS format

--- scripts/fix-task-a-01-label.py
+++ scripts/fix-task-a-01-label.py
@@ -10,13 +10,22 @@
 
 import sys
 
+# fix header
+header = sys.stdin.readline().rstrip().split(',')
+assert header[0] == 'rewire_id'
+assert 'label' in header[-1].lower()
+print('rewire_id,label_pred')
+
+# fix body
 while True:
     line = sys.stdin.readline()
     if not line:
         break
     fields = line.rstrip().split(',')
     if fields[-1] == '0':
-        fields[-1] = 'not sexist'
+        label = 'not sexist'
     elif fields[-1] == '1':
-        fields[-1] = 'sexist'
-    print(','.join(fields))
+        label = 'sexist'
+    else:
+        label = fields[-1]
+    print('%s,%s' %(fields[0], label))
