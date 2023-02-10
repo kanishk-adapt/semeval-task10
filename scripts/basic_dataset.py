a022f728f4abdb51df29882f40ba7a7431832472
Joachim Wagner <jowagner@users.noreply.github.com>
2023-02-10 17:15:03 +0000

    simplify code

--- scripts/basic_dataset.py
+++ scripts/basic_dataset.py
@@ -49,7 +49,7 @@ class Item:
             return None
         text = []
         info = []
-        for seq_idx, doc_idx in enumerate(self.documents):
+        for _, doc_idx in enumerate(self.documents):
             document = self.dataset.documents[doc_idx]
             if get_text: text.append(document)
             if get_info: info.append((doc_idx, len(document.split())))
@@ -261,8 +261,7 @@ class Dataset(collections.Sequence):
             assert not self.items  # assumes we cannot delete documents
             return
         self.items = []
-        if self.deduplicate:
-            self.hash2item_idxs = {}
+        self.hash2item_idxs = {}
         if self.unit == 'class':
             self._reset_items_for_per_class_mode()
         elif self.unit == 'document':
